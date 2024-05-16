a4585bc5f889a912fcb994b5b424189960a098e3
heloiserodrigues <111513024+heloiserodrigues@users.noreply.github.com>
2024-05-16 10:17:58 -0300

    ok

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-12-14T00:00:00");
 const tempoObjetivo2 = new Date("2024-12-30T00:00:00");
 const tempoObjetivo3 = new Date("2024-11-30T00:00:00");
-const tempoObjetivo4 = new Date("2026-06-08T00:00:00");
+const tempoObjetivo4 = new Date("2026-09-30T00:00:00");
 //constante da lista dos tempos de objetivo
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
